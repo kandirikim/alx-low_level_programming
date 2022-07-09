18ac73423bb9557db48647aaf20e23a37ef804c7
kandirikim <peterkimbeghi@gmail.com>
2022-07-08 19:30:08 -0700

    chars errror corrected

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -2,14 +2,21 @@
 
 /**
  *main -> assign a random number to the variable n each time it is executed
- *
+ *and print the last digit of the number stored in the variable n
  *Return: Always 0 (Success)
  */
 int main(void)
 {
-char ch;
-for (ch = 'a'; ch <= 'z'; ch++)
+int ch;
 
+for (ch = 97; ch <= 122; ch++)
+{
+putchar(ch);
+}
+for (ch = 65; ch <= 90; ch++)
+{
+putchar(ch);
+}
 putchar(10); /* this is an ascii code for new line*/
 
 return (0);
